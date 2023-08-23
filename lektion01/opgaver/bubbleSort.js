4c8e921b180d352900277b6e41f98d2d9a8ea65c
AndersMikkelsen <ande077h@gmail.com>
2023-08-23 09:39:59 +0200

    opgave 1.3 donzo

--- lektion01/opgaver/bubbleSort.js
+++ lektion01/opgaver/bubbleSort.js
@@ -1,4 +1,4 @@
-
+//Opgave 1.2
 let list1 = ["a", "c", "b", "d", "f", "e"];
 for (let i = list1.length - 1; i >= 0; i--) {
     for (let j = 0; j <= i - 1; j++) {
@@ -10,3 +10,29 @@ for (let i = list1.length - 1; i >= 0; i--) {
     }
 }
 console.log(list1.toString()); // => a,b,c,d,e,f
+
+
+
+//Opgave 1.3 Binary Search
+function binarySearch(list, target){
+    let index = -1;
+    let left = 0;
+    let right = list.length - 1;
+    while (index === -1 && left <= right){
+        let middle = Math.floor((left + right) / 2);
+        if (list[middle] === target){
+            index = middle;
+        } else if (list[middle] > target){
+            right = middle - 1;
+        }   else {
+            left = middle + 1;
+        }
+    }
+    return index;
+}
+
+//returnere 1 hvis tallet findes, returnere -1 hvis tallet ikke findes
+console.log(binarySearch(list1,"b"))
+
+
+
